63bd3e55652fa18beb51d26b961103dd75a74c5c
kmoroz <ksmorozova@hotmail.com>
2021-03-01 19:36:46 +0000

    add bash comment on top

--- srcs/setup_database.sh
+++ srcs/setup_database.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 service mysql start
 
 # Configure a wordpress database
@@ -6,3 +8,6 @@ echo "CREATE USER 'ksenia'@'localhost';" | mysql -u root
 echo "SET PASSWORD FOR 'ksenia'@'localhost' = PASSWORD('12345678');" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'ksenia'@'localhost' IDENTIFIED BY '12345678';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
+
+wp core install --path='/var/www/codam/wordpress' --allow-root --url="/"  --title="Welcome to ft_shrek!" --admin_user="ksenia" --admin_password="12345678" --admin_email="root@gmail.com" && \
+    mysql -e "USE wordpress;UPDATE wp_options SET option_value='https://localhost/wordpress' WHERE option_name='siteurl' OR option_name='home';"
