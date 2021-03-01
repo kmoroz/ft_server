13653ad1dbb4a4372a88963d3fa9b672f7fd26c5
kmoroz <ksmorozova@hotmail.com>
2021-03-01 19:34:19 +0000

    add bash comment on top and swap container setup echo with nginx

--- srcs/configure_container.sh
+++ srcs/configure_container.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 openssl req -newkey rsa:2048 -x509 -sha256 -days 42 -nodes -out codam.crt -keyout codam.key -subj "/C=IE/ST=Leinster/L=Dublin/O=Codam/CN=codam"
 mv codam.crt /etc/ssl/certs/
 mv codam.key /etc/ssl/private/
@@ -5,9 +7,5 @@ mv codam.key /etc/ssl/private/
 service mysql start
 service php7.3-fpm start
 
+echo "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  Container setup finished hurray!	(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
 nginx -g "daemon off;" 
-echo "container setup finished hurray"
-
-
-
-
